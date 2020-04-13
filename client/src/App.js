63b5f61de460c1b5e60187d44e79d4f3207696be
Jeffrey Soriano <jeffsor17@gmail.com>
2020-04-13 11:14:05 -0600

    axios index call working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, } from "react";
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [items, setItems] = useState([])
+  //item = current state
+  //setItem = is like setState({})
+  useEffect(() => {
+    axios.get(`/api/items`).then(res => {
+      setItems(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  const renderItems = () => {
+    return items.map(item => (
+      <div key={`${item.id}`}>
+        <img src={item.image}/>
+        <p>{item.name}</p>
+        <p>{item.description}</p>
+        <p>{item.likes}</p>
+      </div>
+    ))
+  }
+
   return (
-   <div>test</div>
+    <>
+      {renderItems()}
+    </>
   );
 }
 
---
+++ client/src/components/ItemForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import { Form } from 'antd';
+
+const ItemForm = () => {
+    return(
+        <>
+            <Form>
+                
+            </Form>
+        </>
+    )
+}
