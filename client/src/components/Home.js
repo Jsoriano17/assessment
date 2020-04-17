5573a8498e668346a4ce6aee34511d3d74f0bd7c
Jeffrey Soriano <jeffsor17@gmail.com>
2020-04-17 01:00:35 -0600

    working on sorting by likes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, } from "react";
 import axios from 'axios';
-import { Card } from 'antd';
+import { Card, Button } from 'antd';
+import styled from 'styled-components';
 
 function Home() {
     const [items, setItems] = useState([])
     const { Meta } = Card;
+    
     useEffect(() => {
         axios.get(`/api/items`).then(res => {
             setItems(res.data)
@@ -12,6 +14,24 @@ function Home() {
             console.log(err)
         })
     }, [])
+
+    const mostLiked = () => {
+        console.log('clicked')
+        let sortedArr = items.sort(function (a, b) {
+            return a.likes - b.likes;
+        });
+        //needs to update?
+        return setItems(sortedArr)
+    }
+
+    const leastLiked = () => {
+        console.log('clicked')
+        let sortedArr = items.sort(function (a, b) {
+            return b.likes - a.likes;
+        });
+        return setItems(sortedArr)
+
+    }
 
     const renderItems = () => {
         return items.map(item => (
@@ -22,7 +42,7 @@ function Home() {
                 cover={<img alt="example" src={item.image} />}
             >
                 <Meta title={item.name} description={item.description} />
-                <br/>
+                <br />
                 <p>{item.likes}</p>
             </Card>
         ))
@@ -30,11 +50,20 @@ function Home() {
 
     return (
         <>
+            <StyledCon>
+                <Button onClick={mostLiked}>Most Popular</Button>
+                <Button onClick={leastLiked}>Least Popular</Button>
+            </StyledCon>
             {renderItems()}
         </>
     );
 }
 
 export default Home;
+
+const StyledCon = styled.div`
+position: absolute; 
+top: 55px;
+`
 
 
