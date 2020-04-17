bb471dcd4ada62431eda66e1c4443a27b1cf28dc
Jeffrey Soriano <jeffsor17@gmail.com>
2020-04-17 02:59:55 -0600

    working sort

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 
 function Home() {
     const [items, setItems] = useState([])
-    const { Meta } = Card;
+    const [most, setMost] = useState(false)
+    const [least, setLeast] = useState(false)
+    const { Meta } = Card; 
 
     useEffect(() => {
         axios.get(`/api/items`).then(res => {
@@ -16,25 +18,69 @@ function Home() {
         })
     }, [])
 
-    const mostLiked = () => {
-        console.log('clicked')
-        let sortedArr = items.sort(function (a, b) {
-            return a.likes - b.likes;
-        });
-        //needs to update?
-        return setItems(sortedArr)
-    }
+    const showMost = () => (
+         setMost(!most),
+         setLeast(false)
+    )
+
+    const showLeast = () => (
+        setLeast(!most),
+         setMost(false)
+    )
 
     const leastLiked = () => {
         console.log('clicked')
         let sortedArr = items.sort(function (a, b) {
-            return b.likes - a.likes;
-        });
-        return setItems(sortedArr)
+            return a.likes - b.likes;
+        })
+        return sortedArr.map(item => (
+            <>
+                <Link to = {{pathname: '/item/show', id: item.id}}>
+                    <Card
+                        key={`${item.id}`}
+                        hoverable
+                        style={{ width: 240, margin: '15px 20px' }}
+                        cover={<img alt="example" src={item.image} />}
+                    >
+                        <Meta title={item.name} description={item.description} />
+                        <br />
+                        <p>Likes: {item.likes}</p>
+                    </Card>
+                </Link>
+            </>
+        ))
+    }
 
+    const mostLiked = () => {
+        console.log('clicked')
+        let sortedArr = items.sort(function (a, b) {
+            return b.likes - a.likes;
+        })
+        return sortedArr.map(item => (
+            <>
+                <Link to = {{pathname: '/item/show', id: item.id}}>
+                    <Card
+                        key={`${item.id}`}
+                        hoverable
+                        style={{ width: 240, margin: '15px 20px' }}
+                        cover={<img alt="example" src={item.image} />}
+                    >
+                        <Meta title={item.name} description={item.description} />
+                        <br />
+                        <p>Likes: {item.likes}</p>
+                    </Card>
+                </Link>
+            </>
+        ))
     }
 
     const renderItems = () => {
+        //render items by array index not item id? 
+        //once i realised that my sortedArr was working i discovered that this is rendering the 
+        //items by id and not index so i took a longer route and made contionals to render not sure if theres
+        //a better way 
+        //when sorting the index was being sorted corretly with the most/lease likes in console
+        //still rendering by the item id
         return items.map(item => (
             <>
                 <Link to = {{pathname: '/item/show', id: item.id}}>
@@ -46,20 +92,30 @@ function Home() {
                     >
                         <Meta title={item.name} description={item.description} />
                         <br />
-                        <p>{item.likes}</p>
+                        <p>Likes: {item.likes}</p>
                     </Card>
                 </Link>
             </>
         ))
     }
 
+    const showByLikes = () => {
+        if( most === false && least === false){
+            return renderItems()
+        } else if (most === true && least === false ){
+            return mostLiked()
+        } else {
+            return leastLiked()
+        }
+    }
+
     return (
         <>
             <StyledCon>
-                <Button onClick={mostLiked}>Most Popular</Button>
-                <Button onClick={leastLiked}>Least Popular</Button>
+                <Button onClick={showMost}>Most Popular</Button>
+                <Button onClick={showLeast}>Least Popular</Button>
             </StyledCon>
-            {renderItems()}
+            {showByLikes()}
         </>
     );
 }
